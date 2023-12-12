84cb175fb7de503e543552d4b2839c32f0e9afda
MZAINR <MZAINR>
2023-12-12 16:12:08 +0700

    menambahkan seluruh file project

---
+++ webpack.common.js
@@ -0,0 +1,85 @@
+/* eslint-disable global-require */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/js/main.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'cuaca.html',
+      template: path.resolve(__dirname, 'src/cuaca.html'),
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'aboutus.html',
+      template: path.resolve(__dirname, 'src/aboutus.html'),
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'donasi.html',
+      template: path.resolve(__dirname, 'src/donasi.html'),
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'detail-bencana.html',
+      template: path.resolve(__dirname, 'src/detail-bencana.html')
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+  ],
+};
